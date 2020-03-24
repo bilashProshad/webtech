488ee328d48ca2e3111f3becee043e671923c917
bilashProshad <pbilash64@gmail.com>
2020-03-25 00:02:13 +0600

    newsfeed updated

--- newsfeed.php
+++ newsfeed.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Online Tutor Finder</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+    <nav>
+        <label class="logo">
+            HotelBooking.com
+        </label>
     
+        <ul>
+            <li><a class="active" href="">News Feed</a></li>
+            <li><a href="">Search</a></li>
+            <li><a href="">Post</a></li>
+            <li><a href="">Notification</a></li>
+            <li><a href="">User</a></li>
+            <li><a href="">Logout</a></li>
+        </ul>
+    
+    </nav>
 </body>
 </html>
