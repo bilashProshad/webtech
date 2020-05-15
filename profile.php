ffb33096a0fbbb35fed5a8865521620f6944447f
bilashProshad <pbilash64@gmail.com>
2020-05-15 20:36:26 +0600

    profile update

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 	
 </head>
-<body>
+<body class="outer">
     <div class="header">
         <h1>Online Tutor Finder</h1>
         <h4>Learn Teach Earn</h4>
@@ -30,41 +30,84 @@
         </ul>
     
     </nav>
-	<form name="search_form_results"  action="UpdateProfileInfo.php" method="POST" class="Timeline-form">
-	<table>
-	<tr>
-                <th>
-                    <h3 style="margin-bottom: 5px"><label for="">Timeline</label> </h3>
-                </th>
-            </tr>
-	   <tr>
-                <th>
-                    <hr>
-                </th>
-            </tr>
-			  </tr>
-			
-            <tr>
-				<th align="center"><button type="submit" name="Update Tution Information" >Update Personal Information</button></th>
-            </tr>
-			  </tr>
-			  </table>
-			  <table>
-			
-            <tr>
-                <th align="center"><button type="submit" name="Update Tution Information"onclick="TutionUpdate();">Update Tution Information</button></th>
-			
-			</tr>
-			  </tr>
-			  </table>
-			<table>
-            <tr>
-                <th align="center"><button type="submit" name="Account Settings"onclick="AccountSettings();">Account Settings</button></th>
-            </tr
-			
-			</table>
+	
+    <form name="search_form_results"  action="#" method="POST" class="timeline">
+    <table class="proTable">
+        <tr>
+            <th><label for="timeline">Timeline</label></th>
+        </tr>
+        <tr>
+            <th><button onclick="profileBtn();" class="active" type="submit" name="btnPersonalInfo" >Personal Information</button></th>
+        </tr>
+        <tr>
+            <th><button onclick="tutioninfoBtn();" type="submit" name="btnTutionInfo">Tution Information</button></th>
+        </tr>
+        <tr>
+            <th><button onclick="acSetBtn();" type="submit" name="btnAccountSet">Account Settings</button></th>
+        </tr>
+    </table>
+    </form>
 
-			</form>
-				<script src="script.js"> </script>
+    <form  action="" method="POST" class="inner" enctype="multipart/form-date">
+	<table>
+        <tr>
+            <th>
+                <h1 style="margin-bottom: 10px"><label for="">User Profile</label></h1>
+            </th>
+        </tr>
+        <tr>
+            <th align="center">
+                <img src="placeholder.png" align="center" onclick="triggerClick()" id="ProfileDisplay"> 
+                    <input type="file" name="ProfileImage" onchange="displayImage(this)" id="ProfileImage" style="display:none;" ><br>
+            </th>
+        </tr>
+        <tr>
+            <th>
+                <input type="text" name="Name" id="Name" placeholder="Name">
+                <span id="Name"></span></br> 
+            </th>
+        </tr>
+        <tr>
+            <th>
+                <input type="text" name="txtEmail" id="txtEmail" placeholder="Email">
+                <span id="userMail"></span> </br> 
+            </th>
+        </tr>
+        <tr>
+            <th>
+                <input type="Number" name="phone Number" id="phone Number" placeholder="phone Number">
+                <span id="phone Number"></span></br>
+            </th>
+        </tr>
+        <tr>
+            <th align="left">
+                <label for="">Gender : &nbsp;</label>
+                <input type="radio" name="gender" id="" value="male">
+                <label for="">Male</label>
+                <input type="radio" name="gender" id="" value="female">
+                <label for="">Female</label>
+                <span id="checkGender"></span>                
+            </th>
+        </tr>
+        <tr>
+            <th>
+                <input type="text" name="Address" id="Address" placeholder="Address">
+                <span id="Address"></span> </br> 
+            </th>
+        </tr>
+        <tr>
+            <th>
+                <input type="text" name="Institution" id="Institution" placeholder="Institution">
+                <span id="Institution"></span> 
+            </th>
+        </tr>
+        <tr>            
+            <th align="center"><button type="submit">Update</button></th>
+        </tr>
+    </table>
+	</form>
+    
+            
+	<script src="js/script.js"> </script>
 </body>
 </html>
